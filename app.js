a85ae756ec6771174d4723513ffce044052f4976
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-06-23 16:10:09 -0400

    Successful test of data model on Firestore.

--- app.js
+++ app.js
@@ -1,174 +1,179 @@
-$(function() {
-  // fb
-  var firebaseConfig = {}
+$(function () {
+  // My Firebase configuration
+  var firebaseConfig = {
+// ENV_VARIABLES
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-  // firebase.initializeApp(firebaseConfig);
+  var DB = firebase.firestore();
+  var testData = DBNAME
 
-  // var DB = firebase.firestore();
-  // var BOARDREF = DB.collection(DBNAME).doc(GAMENAME).collection('board').doc('board');
+  var BOARDREF = DB.collection(DBNAME).doc(GAMENAME).collection('board').doc('board');
+  var testBoard = DB.collection(DBNAME).doc(GAMENAME).collection('board')
+  console.log("I am testBoard:", testBoard.data)
+
   var PLAYERSREF;
   var PLAYERSUNSUBSCRIBE = null;
-  var UID = Math.round(Math.random() * 1000000)
-  var PLAYERDIVS = {}
-  var POS = [0, 0]
+  var UID = Math.round(Math.random() * 1000000);
+  var PLAYERDIVS = {};
+  var POS = [0, 0];
   var GRIDSIZE;
-  var MAZE = new Maze.Maze()
+  var MAZE = new Maze.Maze();
   var CURRENTSEED;
-  var SIZE = 16
-  var BOARDSIZE = 600
+  var SIZE = 20;
+  var BOARD_SIZE = 640;
 
   function monitorBoard() {
     BOARDREF.onSnapshot(function (doc) {
-      if (doc.data() === undefined) {
-        startNewGame()
+      if (doc.data() == undefined) {
+        startNewGame();
         return;
       }
-      clearMaze()
-      CURRENTSEED = doc.data().seed
-      monitorPlayers()
-      MAZE.generate(CURRENTSEED, SIZE)
-      MAZE.draw($("#canvas"))
-    })
+      clearMaze();
+      CURRENTSEED = doc.data().seed;
+      console.log("I am new game doc.data", doc.data())
+      monitorPlayers();
+      MAZE.generate(CURRENTSEED, SIZE);
+      MAZE.draw($("#canvas"));
+    });
   }
 
   function drawPlayers(players) {
     for (var uid in players) {
-      if (!uid.match(/^\d+$/)) continue
+      if (!uid.match(/^\d+$/)) continue;
 
       if (uid == UID) {
-        updatePlayerLocation(UID, POS, false)
+        updatePlayerLocation(UID, POS, false);
       } else {
-        updatePlayerLocation(uid, players[uid], false)
+        updatePlayerLocation(uid, players[uid], false);
       }
     }
   }
 
   function updatePlayerLocation(uid, coord, write = true) {
     if (!PLAYERDIVS[uid]) {
-      // create new player
-      var div = $("<div id=player" + uid + " class=player style='background-color:" + getRandomColor(uid) + "'></div>")
-      $("#players").append(div)
-      PLAYERSDIV[uid] = div
+      // create player
+      var div = $("<div id=player" + uid + " class=player style='background-color:" + getRandomColor(uid) + "'></div>");
+      $("#players").append(div);
+      PLAYERDIVS[uid] = div;
     }
-  }
 
-  var circleSize = 10
-  var nudge = GRIDSIZE * 0.25
-  var offsetx = nudge - 2 * srand(uid) * nudge - circleSize / 2 + GRIDSIZE / 2;
-  var offsety = nudge - 2 * srand(uid + 1) * nudge - circleSize / 2 + GRIDSIZE / 2;
-  var left = coord[0] * GRIDSIZE + offsetx
-  var top = coord[1] * GRIDSIZE + offsety
-  var css = { 
-    left: left,
-    top: top,
-    width: circleSize,
-    height: circleSize
-  }
-  PLAYERDIVS[uid].css(css)
-  if (wright && uid == UID) {
-    DB.runTransaction(function (transaction) {
-      return transaction.get(PLAYERSREF).then(function (doc) {
-        // add Self to players
-        var players = doc.data()
-        if (!players) players = {}
-        players[UID] = POS
-        transaction.set(PLAYERSREF, players)
-      })
-    }).then(function () {
-      console.log("Transaction committed!")
-    }).catch(function (error) {
-      console.log("Transaction error:", error)
-    })
+    var circleSize = 10;
+    var nudge = GRIDSIZE * 0.18; // offset the player a bit randomly
+    var offsetx = nudge - 2 * srand(uid) * nudge - circleSize / 2 + GRIDSIZE / 2;
+    var offsety = nudge - 2 * srand(uid + 1) * nudge - circleSize / 2 + GRIDSIZE / 2;
+    var left = coord[0] * GRIDSIZE + offsetx;
+    var top = coord[1] * GRIDSIZE + offsety;
+    var css = { left: left, top: top, width: circleSize, height: circleSize };
+    PLAYERDIVS[uid].css(css);
+    if (write && uid == UID) {
+      DB.runTransaction(function (transaction) {
+        return transaction.get(PLAYERSREF).then(function (doc) {
+          // add self to players.
+          var players = doc.data();
+          console.log("I am players:", players)
+
+          if (!players) players = {};
+          players[UID] = POS;
+          transaction.set(PLAYERSREF, players);
+        });
+      }).then(function () {
+        console.log("Transaction successfully committed!");
+      }).catch(function (error) {
+        console.log("Transaction failed: ", error);
+      });
+    }
   }
 
   function monitorPlayers() {
-    if (PLAYERSUNSUBSCRIBE) {
-      PLAYERSUNSUBSCRIBE();
-      PLAYERSUNSUBSCRIBE = null;
-    }
-    PLAYERSREF = DB.collection(DBNAME).doc(GAMENAME).collection('players').doc('players' + CURRENTSEED)
+    if (PLAYERSUNSUBSCRIBE) { PLAYERSUNSUBSCRIBE(); PLAYERSUNSUBSCRIBE = null; }
+    PLAYERSREF = DB.collection(DBNAME).doc(GAMENAME).collection('players').doc('players' + CURRENTSEED);
+    var testPlayers = DB.collection(DBNAME).doc(GAMENAME).collection('players')
+    console.log("I am PLAYERS:", testPlayers)
+    console.log("I am CURRENTSEED:", CURRENTSEED)
+
     PLAYERSUNSUBSCRIBE = PLAYERSREF.onSnapshot(function (doc) {
-      drawPlayers(doc.data())
-    })
-    updatePlayerLocation(UID, POS)
+      drawPlayers(doc.data());
+    });
+
+    updatePlayerLocation(UID, POS);
   }
 
   function clearMaze() {
-    $("#players").empty()
-    var star = $(`<div class="player">&#x2605;</div>`)
-    star.css({left: BOARD_SIZE - GRIDSIZE/2 - 5, top: BOARD_SIZE - GRIDSIZE/2 - 5, border:0})
-    $("#players").append(star)
-    PLAYERDIVS = {}
-    POS = [0, 0]
+    $("#players").empty();
+    var star = $("<div class=player>&#x2605;</div>");
+    star.css({left: BOARD_SIZE - GRIDSIZE/2 - 5, top: BOARD_SIZE - GRIDSIZE/2 - 5, border:0});
+    $("#players").append(star);
+    PLAYERDIVS = {};
+    POS = [0, 0];
   }
 
   function startNewGame() {
-    BOARDREF.get().then(function () {
-      clearMaze()
+    BOARDREF.get().then(function (doc) {
+      clearMaze();
 
-      var seed = 0
+      var seed = 0;
       if (doc.data()) {
-        seed = doc.data().seed
+        seed = doc.data().seed;
       }
-      seed++
-      BOARDREF.set({
-        'seed': seed
-      })
-    })
+      seed++;
+      BOARDREF.set({ 'seed': seed });
+    });
   }
 
-  window.goRight = function() {
+  window.goRight = function () {
     if (!MAZE.hasWall(POS, Maze.RIGHT)) {
-      POS[0]++
-      updateMyPosition()
+      POS[0]++;
+      updateMyPosition();
     }
   }
-  window.goDown = function() {
+  window.goDown = function () {
     if (!MAZE.hasWall(POS, Maze.BOTTOM)) {
-      POS[1]++
-      updateMyPosition()
+      POS[1]++;
+      updateMyPosition();
     }
   }
-  window.goLeft = function() {
+  window.goLeft = function () {
     if (!MAZE.hasWall(POS, Maze.LEFT)) {
-      POS[0]--
-      updateMyPosition()
+      POS[0]--;
+      updateMyPosition();
     }
   }
-  window.goUp = function() {
+  window.goUp = function () {
     if (!MAZE.hasWall(POS, Maze.TOP)) {
-      POS[1]--
-      updateMyPosition()
+      POS[1]--;
+      updateMyPosition();
     }
   }
 
   function updateMyPosition() {
-    updatePlayerLocation(UID, POS)
+    updatePlayerLocation(UID, POS);
     if (POS[0] == SIZE - 1 && POS[1] == SIZE - 1) {
-      setTimeout(function () {
-        alert("You won!")
-        startNewGame()
-      }, 100)
+      setTimeout(function(){
+        alert("You won!");
+        startNewGame();
+      }, 100);
     }
   }
 
-  function srand() {
-    var x = Math.sin(seed) * 10000
-    return x - Math.floor(x)
+  function srand(seed) {
+    var x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
   }
 
-// not using getRandomColor function yet
-// function getRandomColor(seed) {
-//   var letters = '0123456789ABCDEF';
-//   var color = '#';
-//   for (var i = 0; i < 6; i++) {
-//     color += letters[Math.floor(srand(seed) * 16)];
-//     ++seed;
-//   }
-//   return color;
-// }
+  function getRandomColor(seed) {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(srand(seed) * 16)];
+      ++seed;
+    }
+    return color;
+  }
 
-  // Key listeners
+  // Register key listeners
   $(document).keydown(function (e) {
     switch (e.keyCode) {
       case 37: //left
@@ -186,14 +191,14 @@ $(function() {
       default:
         return;
     }
+    // Disable document scrolling.
+    e.preventDefault();
+  });
 
-    e.preventDefault()
-  })
-
-  var canvas = $("#canvas")
-  canvas.attr('width', BOARD_SIZE)
-  canvas.attr('height', BOARD_SIZE)
-  var c = canvas.get(0).getContext("2d")
-  GRIDSIZE = canvas.width() / SIZE
-  monitorBoard()
-})
+  var canvas = $("#canvas");
+  canvas.attr('width', BOARD_SIZE);
+  canvas.attr('height', BOARD_SIZE);
+  var c = canvas.get(0).getContext("2d");
+  GRIDSIZE = canvas.width() / SIZE;
+  monitorBoard();
+});
--- maze.js
+++ maze.js
@@ -1,87 +1,88 @@
-var Maze = (function() {
-  var LEFT = 0b0001 // 1
-  var RIGHT = 0b0010 // 2
-  var BOTTOM = 0b0100 // 4
-  var TOP = 0b1000 // 8
-
+var Maze = (function () {
+  var LEFT = 0b0001; //1
+  var RIGHT = 0b0010; //2
+  var BOTTOM = 0b0100; //4
+  var TOP = 0b1000; //8
   var MAZE_SIZE;
 
-  function srand() {
-    var x = Map.sin(seed) * 10000
-    return x - Math.floor(x)
+  function srand(seed) {
+    var x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
   }
 
   class Maze {
     generate(seed, size) {
-      MAZE_SIZE = size
-      this.currentSeed = seed
-      this.maze = new Array(MAZE_SIZE)
+      MAZE_SIZE = size;
+      this.currentSeed = seed;
+      this.maze = new Array(MAZE_SIZE);
 
-      var maze = this.maze
-      var seen = {}
+      var maze = this.maze;
+      var seen = {};
       for (var i = 0; i < MAZE_SIZE; ++i) {
-        maze[i] = new Array(MAZE_SIZE)
+        maze[i] = new Array(MAZE_SIZE);
         for (var j = 0; j < MAZE_SIZE; ++j) {
           maze[i][j] = LEFT | TOP | BOTTOM | RIGHT;
-          seen[this._hashKeyForCoord([i, j])] = false
+          seen[this._hashKeyForCoord([i, j])] = false;
         }
       }
 
-      var stack = []
-      var coord = [0, 0]
+      var stack = [];
+      var coord = [0, 0];
       var nextCoord;
-      var seenCount = 0
+      var seenCount = 0;
       while (seenCount < MAZE_SIZE * MAZE_SIZE) {
         if (!seen[this._hashKeyForCoord(coord)]) {
-          ++seenCount
+          ++seenCount;
         }
-        seen[this._hashKeyForCoord(coord)] = true
+        seen[this._hashKeyForCoord(coord)] = true;
 
         var brokeWall = this._breakRandomWall(maze, coord, seen);
         if (brokeWall) {
-          stack.push(coord)
-          nextCoord = this._coordForDirection(coord, brokeWall)
+          stack.push(coord);
+          nextCoord = this._coordForDirection(coord, brokeWall);
         } else {
-          nextCoord = stack.pop()
+          nextCoord = stack.pop();
         }
-        coord = nextCoord
+        coord = nextCoord;
       }
     }
 
     draw(canvas) {
-      var width = canvas.width()
-      var c = canvas.get(0).getContext("2d")
+      var width = canvas.width();
+      var c = canvas.get(0).getContext("2d");
 
-      var gridSize = width / MAZE_SIZE
-      c.clearRect(0, 0, canvas.width(), canvas.height())
+      var gridSize = width / MAZE_SIZE;
+      c.clearRect(0, 0, canvas.width(), canvas.height());
 
-      c.beginPath()
-      c.moveTo(0, gridSize)
+      c.beginPath();
+      c.moveTo(0, gridSize);
       for (var y = 0; y < MAZE_SIZE; ++y) {
         for (var x = 0; x < MAZE_SIZE; ++x) {
           var item = this.maze[y][x];
           if ((item & BOTTOM) > 0) {
-            c.moveTo(x * gridSize, (y + 1) * gridSize)
-            c.lineTo((x + 1) * gridSize, (y + 1) * gridSize)
+            c.moveTo(x * gridSize, (y + 1) * gridSize);
+            c.lineTo((x + 1) * gridSize, (y + 1) * gridSize);
           }
           if ((item & RIGHT) > 0) {
-            c.moveTo((x + 1) * gridSize, (y) * gridSize)
-            c.lineTo((x + 1) * gridSize, (y + 1) * gridSize)
+            c.moveTo((x + 1) * gridSize, (y) * gridSize);
+            c.lineTo((x + 1) * gridSize, (y + 1) * gridSize);
           }
         }
       }
-      c.stroke()
+      c.stroke();
     }
 
     hasWall(pos, direction) {
-      var walls = this.maze[pos[1]][pos[0]]
-      return (walls * direction) > 0
+      console.log("I am maze:", this.maze)
+
+      var walls = this.maze[pos[1]][pos[0]];
+      return (walls & direction) > 0;
     }
 
-    _hashKeyForCoord(c) { return c[0] + ',' + c[1] }
+    _hashKeyForCoord(c) { return c[0] + ',' + c[1]; }
 
     _coordForDirection(coord, direction) {
-      var nextCoord = [coord[0], coord[1]]
+      var nextCoord = [coord[0], coord[1]];
       switch (direction) {
         case TOP:
           nextCoord[1]--;
@@ -97,14 +98,13 @@ var Maze = (function() {
           break;
       }
       if (nextCoord[0] < 0 || nextCoord[0] >= MAZE_SIZE ||
-        nextCoord[1] < 0 || nextCoord[1] >= MAZE_SIZE) {
-          return false
-        }
-        return nextCoord
+        nextCoord[1] < 0 || nextCoord[1] >= MAZE_SIZE) return false;
+
+      return nextCoord;
     }
 
     _oppositeDirection(direction) {
-      switch(dirction) {
+      switch (direction) {
         case LEFT: return RIGHT;
         case RIGHT: return LEFT;
         case TOP: return BOTTOM;
@@ -113,20 +113,20 @@ var Maze = (function() {
     }
 
     _breakRandomWall(maze, coord, seen) {
-      var x = coord[0]
-      var y = coord[1]
-      var wals = maze[y][x]
+      var y = coord[1];
+      var x = coord[0];
+      var walls = maze[y][x];
       if (walls > 0) {
-        var dirctions = [LEFT, TOP, BOTTOM, RIGHT]
-        this._shuffleArray(direction)
+        var directions = [LEFT, TOP, BOTTOM, RIGHT];
+        this._shuffleArray(directions);
 
         for (let wall of directions) {
           if (x == 0 && wall == LEFT) continue;
           if (x >= MAZE_SIZE - 1 && wall == RIGHT) continue;
           if (y == 0 && wall == TOP) continue;
           if (y >= MAZE_SIZE - 1 && wall == BOTTOM) continue;
-  
-          var otherWall = this._coordForDirection(coord, wall)
+
+          var otherWall = this._coordForDirection(coord, wall);
           if (!otherWall) continue;
           if (seen[this._hashKeyForCoord(otherWall)]) continue;
           if ((walls & wall) > 0) { // wall is not broken
@@ -142,18 +142,20 @@ var Maze = (function() {
     }
 
     _shuffleArray(array) {
-      for(var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(this._random * (i + 1))
-        var temp = array[1]
-        array[1] = array[j]
+      for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(this._random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
         array[j] = temp;
       }
     }
 
     _random() {
-      return srand(this.currentSeed++)
+      return srand(this.currentSeed++);
     }
   }
+
+  console.log("I am Maze !!", Maze)
 
   return {
     Maze : Maze,
@@ -161,5 +163,6 @@ var Maze = (function() {
     TOP: TOP,
     RIGHT: RIGHT,
     BOTTOM: BOTTOM
-  }
+  };
 })();
+
